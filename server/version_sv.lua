4b1c49f7257ce0c485170188d2b229db847aa615
Bombay <72895600+BombayV@users.noreply.github.com>
2021-05-26 10:59:06 -0500

    refactor(version_sv): Changed name and local var

--- server/version_sv.lua
+++ server/version_sv.lua
@@ -1,8 +1,8 @@
-local name = "[^4pe-hud^7]"
+local name = "[^4not-pe-hud^7]"
 
 AddEventHandler('onResourceStart', function(resourceName)
 	if GetCurrentResourceName() == resourceName then
-		function checkVersion(error, latestVersion, headers)
+		local function checkVersion(error, latestVersion, headers)
 			local currentVersion = LoadResourceFile(resourceName, "version")
 			if currentVersion ~= latestVersion and tonumber(currentVersion) < tonumber(latestVersion) then
 				print(name .. " ^1is outdated.\nCurrent version: ^8" .. currentVersion .. "\nNewest version: ^2" .. latestVersion .. "\n^3Update^7: https://github.com/Project-Entity/pe-hud")

b7c308703863029d0e149b6f7d4acad3058ede19
MSRP <OMGmsebastian@gmail.com>
2021-06-18 01:31:31 -0400

    New version

--- server/version_sv.lua
+++ server/version_sv.lua
@@ -2,11 +2,11 @@ local name = "[^4ev-hud^7]"
 
 AddEventHandler('onResourceStart', function(resourceName)
     if GetCurrentResourceName() == resourceName then
-        local function checkVersion(error, latestVersion, headers)
+        local function checkVersion(e, latestVersion, headers)
             local currentVersion = LoadResourceFile(resourceName, "version")
             if currentVersion ~= latestVersion and tonumber(currentVersion) < tonumber(latestVersion) then
                 print(name .. " ^1is outdated.\nCurrent version: ^8" .. currentVersion ..
-                "\nNewest version: ^2" .. latestVersion .. "\n^3Update^7: https://github.com/EntityEvolution/ev-hud")
+                "\nNewest version: ^2" .. latestVersion .. "\n^3Update^7: https://github.com/EntityEvolution/ev-hud/releases")
             elseif tonumber(currentVersion) > tonumber(latestVersion) then
                 print(name .. " has skipped the latest version ^2" .. latestVersion..
                 ". Either Github is offline or the version file has been changed")

5e580eeca2967219513e4b7ca58d742b9453175d
XiosBombay <OMGmsebastian@gmail.com>
2021-08-17 23:05:55 -0500

    feat(client/voice): implementation of pma-voice

--- client/hud_cl.lua
+++ client/hud_cl.lua
@@ -211,10 +211,22 @@ if not Config.usePMAvoice then
 else
 	function currentVoiceMode(value)
 		print(value)
-		SendNUIMessage({
-			action = 'voiceMode',
-			microphone = value
-		})
+		if (value == 1) then
+			SendNUIMessage({
+				action = 'voiceMode',
+				microphone = 33
+			})
+		elseif (value == 2) then
+			SendNUIMessage({
+				action = 'voiceMode',
+				microphone = 66
+			})
+		elseif (value == 3) then
+			SendNUIMessage({
+				action = 'voiceMode',
+				microphone = 100
+			})
+		end
 	end
 	exports('currentVoiceMode', value)
 end
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 
 game 'gta5'
 
-version '3.0.0'
+version '3.2.1'
 
 description 'Standalone hud created by a Entity Evolution'
 
